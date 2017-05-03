04b4929166a630ba8817dfa4955592fbee6be06e
Andres Teran <aftosb95>
2017-05-03 15:05:16 -0500

    TRabajo Final Android parte1 logio createaccount

---
+++ app/src/main/java/com/example/andresteran_i014213/projectofinal_sti/CreateAccountActivity.java
@@ -0,0 +1,22 @@
+package com.example.andresteran_i014213.projectofinal_sti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        showTolbar(getResources().getString(R.string.txt_title_toolbar),true);
+    }
+
+    private void showTolbar(String title, boolean upButton) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+}
---
+++ app/src/test/java/com/example/andresteran_i014213/projectofinal_sti/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.andresteran_i014213.projectofinal_sti;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
