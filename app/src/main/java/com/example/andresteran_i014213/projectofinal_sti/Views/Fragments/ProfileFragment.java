29afb8d2e503f6d837ebd62bf1f34004809e1cd2
kenetriascos <kenethriascosbravo@gmail.com>
2017-05-22 16:04:01 -0500

    Perfil de usuario SITP

--- app/src/main/java/com/example/andresteran_i014213/projectofinal_sti/Views/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/andresteran_i014213/projectofinal_sti/Views/Fragments/ProfileFragment.java
@@ -1,11 +1,20 @@
 package com.example.andresteran_i014213.projectofinal_sti.Views.Fragments;
 
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.andresteran_i014213.projectofinal_sti.R;
 
@@ -14,17 +23,35 @@ import com.example.andresteran_i014213.projectofinal_sti.R;
  */
 public class ProfileFragment extends Fragment {
 
+    View view;
 
     public ProfileFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        showTolbar(getResources().getString(R.string.txt_title_toolbar_profile),true);
+        setHasOptionsMenu(true);
+
+        return view ;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showTolbar(String title, boolean upButton) {
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
 }
